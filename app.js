2c5a430d771837a30141b1512dc44c3cc2fef226
CarvalhoLC <carvalho.luisc@gmail.com>
2020-04-12 15:01:35 -0300

    alter app.js / tratamento de erro

--- app.js
+++ app.js
@@ -10,4 +10,22 @@ app.use(morgan('dev'));
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
 
+// Rota não encontrada
+app.use((req, res, next) => {
+    const erro = new Error('Rota não encontrada!');
+
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    });
+});
+
 module.exports = app;
